9ac9ae5233ec6edd14da6f77edb62d1ebc84e608
Erik <erik@Eriks-MacBook-Pro-2.local>
2021-07-01 09:47:02 -0700

    Comment out sameSite: none in app.use session

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ app.use(
     resave: true,
     saveUninitialized: true,
     cookie: {
-      sameSite: "none",
+      // sameSite: "none",
       secure: true,
       maxAge: 1000 * 60 * 5 // 5 minutes
     }
