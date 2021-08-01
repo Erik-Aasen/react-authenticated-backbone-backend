b4707181dad2e477d356863ffc06db03a811896c
Erik <erik@Eriks-MacBook-Pro-2.local>
2021-08-01 14:20:23 -0700

    Set response headers in index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,20 @@ mongoose.connect(`${process.env.PART1}${process.env.USERNAME}:${process.env.PASS
 
 // Middleware
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if ('OPTIONS' == req.method) {
+       res.send(200);
+   } else {
+       next();
+   }
+  });
+
+
 app.use(express.json());
 app.use(cors({ origin: "https://react-auth-backbone-frontend.herokuapp.com", credentials: true }))
 
